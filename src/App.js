2bd32acda92d745421571b3696bc5e5b4633139f
Abhishek Ranjan <64737963+arjha97@users.noreply.github.com>
2021-02-25 15:39:06 +0530

    added navbar and components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+   <div>
+     <Navs/>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+
+        <Route exact path="/starred">
+          <Starred/>
+        </Route>
+
+        <Route>
+          Not Found
+        </Route>
+      </Switch>
+   </div>    
+  )
 }
 
-export default App;
+export default App
+
