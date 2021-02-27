96a5fbd15e8a9d387003e61c1d984ead39aa2317
Abhishek Ranjan <64737963+arjha97@users.noreply.github.com>
2021-02-27 23:57:21 +0530

    added styled part1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import {ThemeProvider} from 'styled-components';
 import Home from './pages/Home';
 import Show from './pages/Show';
 import Starred from './pages/Starred';
 
 
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
+
+
+
 const App = () => {
  return (
+    <ThemeProvider theme={theme}>
       <Switch>
         <Route exact path="/">
           <Home/>
@@ -24,6 +36,7 @@ const App = () => {
           Not Found
         </Route>
       </Switch>   
+    </ThemeProvider> 
   )
 }
 
